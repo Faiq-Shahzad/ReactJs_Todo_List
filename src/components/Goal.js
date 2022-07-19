9fe0ad107d56d80a2a30b6f1bd6621084663b2a8
Faiq Shahzad <f03315558407@gmail.com>
2022-07-19 12:44:15 +0500

    TODO List ReactJs

--- src/components/Goal.js
+++ src/components/Goal.js
@@ -1,27 +1,15 @@
 import React from "react";
-import styled from "styled-components";
+import styles from "./Goal.module.css";
 
 const Goal = (props) => {
   const deleteItemHandler = (id) => {
     props.onDelete(id);
   };
   return (
-    <GoalDiv onClick={() => deleteItemHandler(props.key)}>
+    <div className={styles.goal} onClick={() => deleteItemHandler(props.key)}>
       {props.title}
-    </GoalDiv>
+    </div>
   );
 };
 
 export default Goal;
-
-const GoalDiv = styled.div`
-  background-color: rgb(128, 9, 82);
-  margin-top: 1%;
-  width: 30%;
-  margin-left: auto;
-  margin-right: auto;
-  color: white;
-  font-size: medium;
-  padding: 0.5%;
-  padding-left: 1%;
-`;
